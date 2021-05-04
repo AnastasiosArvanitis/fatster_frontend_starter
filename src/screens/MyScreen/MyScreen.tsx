14426b7679ca45f9c3a77aee470213c0f89b4fd9
Anastasios Arvanitis <62649615+AnastasiosArvanitis@users.noreply.github.com>
2021-05-04 17:56:49 +0200

    passing a type to the state

--- src/screens/MyScreen/MyScreen.tsx
+++ src/screens/MyScreen/MyScreen.tsx
@@ -3,6 +3,7 @@ import { View, Text, StyleSheet, FlatList } from "react-native";
 
 import { useSelector, useDispatch } from "react-redux";
 import { fetchAllUsersRequest } from '../../store/actions/';
+import { IRootState } from "../../store";
 
 interface MyScreenProps {
   //proData: ProModel; TO DO MODEL
@@ -15,21 +16,21 @@ const MyScreen: FC<MyScreenProps> = () => {
     dispatch(fetchAllUsersRequest());
   });
 
-  const users = useSelector(state => state.users.users);
-  const error = useSelector(state => state.users.error);
-
-
-  console.log(`Users : ${users}`);
+  const users = useSelector((state: IRootState) => state.users.users);
+  const error = useSelector((state: IRootState) => state.users.error);
+  const loading = useSelector((state: IRootState) => state.users.loading);
 
   return (
     <View style={styles.container}>
       <Text style={styles.text}>
         Results
       </Text>
-      <Text style={styles.text}>
+      <View>
         {error ? error : null}
-        {users ? users.id : 'no users'}
-      </Text>
+        {users?.length ? users.map( user => {
+          return <View><Text style={styles.text}>{user.name}</Text></View>
+        }) : 'no users'}
+      </View>
     </View>
   );
 }
