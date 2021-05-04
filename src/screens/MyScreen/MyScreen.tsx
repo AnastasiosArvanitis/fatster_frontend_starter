7149137bd9eb48a2c62acd37932cf958f4d64b7f
Anastasios Arvanitis <62649615+AnastasiosArvanitis@users.noreply.github.com>
2021-05-04 12:03:49 +0200

    dispatching in the useEffect of the screen an action

--- src/screens/MyScreen/MyScreen.tsx
+++ src/screens/MyScreen/MyScreen.tsx
@@ -1,14 +1,33 @@
-import React, { FC } from "react";
+import React, { FC, useEffect } from "react";
 import { View, Text, StyleSheet, FlatList } from "react-native";
+
+import { useSelector, useDispatch } from "react-redux";
+import { fetchAllUsersRequest } from '../../store/actions/';
 
 interface MyScreenProps {
   //proData: ProModel; TO DO MODEL
 }
 
 const MyScreen: FC<MyScreenProps> = () => {
+  const dispatch = useDispatch();
+
+  useEffect( () => {
+    dispatch(fetchAllUsersRequest());
+  });
+
+  const users = useSelector(state => state.users.users);
+  const error = useSelector(state => state.users.error);
+
+
+  console.log(`Users : ${users}`);
+
   return (
     <View style={styles.container}>
-      <Text>MyScreen</Text>
+      <Text style={styles.text}>
+        Perdoulas
+        {error ? error : null}
+        {users ? users.id : 'no users'}
+      </Text>
     </View>
   );
 }
@@ -21,4 +40,7 @@ const styles = StyleSheet.create({
     padding:5,
     backgroundColor: '#ffffff'
   },
+  text: {
+    color: '#F00',
+  }
 });
