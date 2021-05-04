d411afa44b9af34d4c157e05f8ec0554f967d330
Anastasios Arvanitis <62649615+AnastasiosArvanitis@users.noreply.github.com>
2021-05-04 17:57:56 +0200

    yield put the action with the payload

--- src/store/sagas/user-saga.ts
+++ src/store/sagas/user-saga.ts
@@ -1,23 +1,21 @@
 import { AnyAction } from "redux";
-import { call, put, takeLatest, takeEvery } from 'redux-saga/effects';
+import { call, put, takeLatest } from 'redux-saga/effects';
 
 import getAllUsers from '../../api/services/users';
 import * as types from '../types/';
+import { fetchAllUsersSuccess, fetchAllUsersFailure } from "../actions";
 
 function* fetchUsers(action: AnyAction) {
   try {
-    const { users } = yield call(getAllUsers);
-    console.log(`user saga fetchUsers users: ${users}`);
-    yield put({ type: types.FETCH_ALL_USERS_SUCCESS, users: action.payload});
+    // @ts-ignore
+    const response: any = yield call(getAllUsers);
+    yield put(fetchAllUsersSuccess(response));
   } catch (error) {
-    yield put({type: types.FETCH_ALL_USERS_FAILURE, error: action.payload})
+    yield put(fetchAllUsersFailure(error.message));
   }
 }
 
 export default function* userSaga() {
-  yield takeEvery(types.FETCH_ALL_USERS_REQUEST, fetchUsers);
+  yield takeLatest(types.FETCH_ALL_USERS_REQUEST, fetchUsers);
 }
 
-/*export default function* Saga() {
-  yield takeLatest(types.FETCH_ALL_USERS_REQUEST, fetchUsers)
-}*/
