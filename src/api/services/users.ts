14b0ea5a6a6ad8f43a30b8495503889b8e51f1dc
Anastasios Arvanitis <62649615+AnastasiosArvanitis@users.noreply.github.com>
2021-05-04 17:58:09 +0200

    refactoring

--- src/api/services/users.ts
+++ src/api/services/users.ts
@@ -4,7 +4,5 @@ const allUsersUrl = '/users';
 
 export default async function getAllUsers() {
   const result = await makeRequest.get(allUsersUrl);
-  const users = result.data;
-  console.log(`Api users: ${users}`);
-  return users;
+  return result.data;
 }
