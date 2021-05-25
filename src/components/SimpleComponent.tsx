718f20aefc8c60b21f7ccb038426342099f8cd74
Anastasios Arvanitis <62649615+AnastasiosArvanitis@users.noreply.github.com>
2021-05-25 17:58:20 +0200

    adding two interfaces just a component

---
+++ src/components/SimpleComponent.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from "react";
+import { View, Text } from "react-native";
+
+const MyScreen: FC<any> = () => {
+  return (
+    <Text>Hello</Text>
+  )
+}
+
+export default MyScreen;
